b530bc2ce5eb79dd6dc0d5185e71eb10dd9eda38
Luo <gnulinux@126.com>
2016-12-01 03:16:13 +0800

    [IMP] impl src/stun/server.rs

--- src/stun/server.rs
+++ src/stun/server.rs
@@ -8,7 +8,10 @@ use std::net::{SocketAddr, IpAddr, TcpListener, TcpStream, UdpSocket, Shutdown};
 use super::{url_parse, STUN_PORT, STUNS_PORT};
 use super::{packet};
 
-pub fn handler(msg: &[u8], response: &mut [u8]) -> Result<usize, ()>{
+pub fn handler(msg: &[u8], response: &mut [u8], 
+    peer_socket_addr: &SocketAddr, local_socket_addr: &SocketAddr) -> Result<usize, ()>{
+    println!("[Handler] Local Addr: {:?} <-- Peer Addr: {:?}", local_socket_addr, peer_socket_addr);
+
     if msg.len() >= 20 {
         match packet::Header::from_bytes(msg) {
             Ok(head) => println!("{:?}", head),
@@ -33,7 +36,8 @@ pub fn tcp_server(host: &str){
                 match stream.read(&mut buf[..]){
                     Ok(size) => {
                         let msg = &buf[..size];
-                        match handler(&msg, &mut response) {
+                        match handler(&msg, &mut response, 
+                            &stream.peer_addr().unwrap(), &socket_addr ) {
                             Ok(size) => {
                                 if size > 0 {
                                     stream.write(&response[..size]);
@@ -58,14 +62,14 @@ pub fn udp_server(host: &str){
     let mut response = [0; 2048];
     loop {
         match socket.recv_from(&mut buf) {
-            Ok((size, src)) => {
-                println!("[INFO] Connection: {:?}", src);
+            Ok((size, peer_socket_addr)) => {
+                println!("[INFO] Connection: {:?}", peer_socket_addr);
                 let msg = &buf[..size];      
                 // thread::spawn(move || handler(&msg, &mut response));
-                match handler(&msg, &mut response) {
+                match handler(&msg, &mut response, &peer_socket_addr, &socket_addr) {
                     Ok(size) => {
                         if size > 0 {
-                            socket.send_to(&response[..size], &src);
+                            socket.send_to(&response[..size], &peer_socket_addr);
                         }
                     },
                     Err(_) => {}
